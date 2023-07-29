e66e475519a4b6f9dc36598ca04839c3e515b677
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-07-29 14:12:52 +0200

    Black format main file

--- core/main.py
+++ core/main.py
@@ -1,9 +1,9 @@
-from twilio.rest import Client
 from core.config import settings
+from twilio.rest import Client
 
 # Your Account SID and Auth Token from console.twilio.com
 ACCOUNT_SID = "ACa96434d632e5d56f486a6504878898a9"
-AUTH_TOKEN  = settings.AUTH_TOKEN
+AUTH_TOKEN = settings.AUTH_TOKEN
 
 client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
@@ -12,6 +12,7 @@ if __name__ == "__main__":
     message = client.messages.create(
         to=f"whatsapp:{settings.NUMBER_TO}",
         from_=f"whatsapp:{settings.NUMBER_FROM}",
-        body="Hello from Python!")
+        body="Hello from Python!",
+    )
 
     print(message.sid)
